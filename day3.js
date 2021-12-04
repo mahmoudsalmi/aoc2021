6c93be97cebe49b82d827adbc337907c4e6643e4
Mahmoud SALMI <mahmoud.salmi@msweb.net>
2021-12-04 23:29:12 +0100

    Day 03 - Js-Part:w2

--- day3.js
+++ day3.js
@@ -20,7 +20,7 @@ async function parseFile(filename) {
   return data;
 }
 
-function solution(data) {
+function mostAndLeast(data) {
   const len = data.length;
   const width = data[0].length;
 
@@ -29,19 +29,38 @@ function solution(data) {
     Array(width).fill(0)
   );
   
-  res = [ res.map(r => r * 2 >= len ? 1 : 0), res.map(r => r * 2 < len ? 1 : 0) ]
+  return [ res.map(r => r * 2 >= len ? 1 : 0), res.map(r => r * 2 < len ? 1 : 0) ]
     .map( bits => bits.reduce((b, s) => b + s, ''))
-    .map(s => parseInt(s, 2));
+}
 
+function solution(data) {
+  const res = mostAndLeast(data).map(s => parseInt(s, 2));
   return [...res, res.reduce((n, m) => n * m, 1)];
 }
 
 function solution2(data) {
-  return `Dummy part2: ${data.length}`;
+  const width = data[0].length;
+  let [most, least] = mostAndLeast(data);
+  let res = [['0'], ['0']]
+  
+  for(let i = 0, mostData = data; i < width && mostData.length > 1; i++) {
+    mostData = mostData.filter(line => String(line[i]) === String(most[i]));
+    [most, ] = mostAndLeast(mostData)
+    res[0] = mostData[0]
+  }
+
+  for(let i = 0, leastData = data; i < width && leastData.length > 1; i++) {
+    leastData = leastData.filter(line => String(line[i]) === String(least[i]));
+    [, least] = mostAndLeast(leastData)
+    res[1] = leastData[0]
+  }
+
+  res = res.map(s => parseInt(s.join(''), 2));
+
+  return [...res, res.reduce((n, m) => n * m, 1)];
 }
 
 (async function() {
-
   console.log("----(AOC2021 - Day 03)--------------------------[Js]----")
   
   const exemple = await parseFile('day3.exemple');
